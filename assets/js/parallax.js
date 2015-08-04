b6db3c565ce9b5c9e6caa581c33d2892dd669536
Juri Schreib <juri@schreib.at>
2015-08-04 19:53:41 +0200

    Update UI

--- assets/js/parallax.js
+++ assets/js/parallax.js
@@ -1,5 +1,5 @@
 
 document.onscroll = function () {
   var el = document.getElementById('page-content');
-  el.style.backgroundPositionY = '-' + document.body.scrollTop/2 + 'px';
+  el.style.backgroundPositionY = '-' + document.body.scrollTop / 1.5 + 'px';
 };
